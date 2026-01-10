641c90a830fcb51c2645bd02c8b48902a695fac7
duckautomata <duckmechatomata@gmail.com>
2026-01-09 22:43:27 -0600

    Fixed build bug

--- vite.config.js
+++ vite.config.js
@@ -37,12 +37,6 @@ export default defineConfig(() => {
                 output: {
                     manualChunks: (id) => {
                         if (id.includes("node_modules")) {
-                            if (id.includes("react") || id.includes("@react-dom")) {
-                                return "react-vendor";
-                            }
-                            if (id.includes("@mui")) {
-                                return "mui-vendor";
-                            }
                             return "vendor";
                         }
                     },
